2337aad2033aa5f8d507eba7c33fb5c857672e7c
Stacey Brogden <toad@brothertoad.net>
2022-07-11 08:58:40 -0700

    Draw knots in blue on test output.

--- beziertest/main.go
+++ beziertest/main.go
@@ -12,14 +12,24 @@ import (
 
 const prefix = `<?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg width="1800px" height="1000px" viewBox="0 0 1800 1000" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink">
+<svg width="1200px" height="800px" viewBox="0 0 1200 800" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink">
   <style>
   * {
     fill: none;
     stroke: black;
     stroke-width: 3;
   }
+  .knot {
+    fill: blue;
+    stroke: blue;
+  }
+  .empty {
+    fill: none;
+    stroke: blue;
+    stroke-width: 2;
+  }
   </style>
+  <rect x="20" y="20" width="1160" height="760" class="empty"/>
 `
 
 const radius = 4
@@ -29,7 +39,7 @@ func main() {
   if len(os.Args) > 1 {
     points = argsToPoints(os.Args[1:])
   } else {
-    points = argsToPoints([]string{ "300,200", "350,344", "388,411", "440,477", "500,500" })
+    points = argsToPoints([]string{ "200,200", "350,344", "388,431", "460,527", "800,400" })
   }
   segments := bezier.SvgControlPointsI(points)
   fmt.Print(prefix)
@@ -37,7 +47,7 @@ func main() {
     fmt.Println(`<path d="`, s, `"/>`)
   }
   for _, p := range(points) {
-    fmt.Printf(`<circle cx="%d" cy="%d" r="%d"/>` + "\n", p.X, p.Y, radius)
+    fmt.Printf(`<circle cx="%d" cy="%d" r="%d" class="knot"/>` + "\n", p.X, p.Y, radius)
   }
   fmt.Println("</svg>")
 }
